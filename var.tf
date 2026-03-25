3e2800f1cdd0623c0400d85d6ccbb98cf9b649f8
Diksha Turkar <turkardiksha345@gmail.com>
2026-03-25 18:45:44 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,24 +1,19 @@
 variable "vpc_cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
 }
 
 variable "public_subnet_cidr" {
-  description = "Public subnet CIDR"
-  type        = string
+
 }
 
 variable "private_subnet_cidr" {
-  description = "Private subnet CIDR"
-  type        = string
+}
+
+variable "private_subnet_cidr_2" {
 }
 
 variable "public_az" {
-  description = "Public subnet AZ"
-  type        = string
 }
 
 variable "private_az" {
-  description = "Private subnet AZ"
-  type        = string
+
 }
